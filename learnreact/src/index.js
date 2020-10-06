0fcf2c48b3513efcc854988098496dee910b421c
mohmad sabban <mohmadsabban123@gmail.com>
2020-10-06 23:23:30 +0530

    introduction of jsx

--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -1,17 +1,27 @@
+// import react lib
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create components
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function components
+
+const App = () => {
+  const labelname = 'Label for Name';
+  return (
+    <div>
+      {/* assign a class name */}
+      <h1 className="heading">Hello function components</h1>
+      {/* inline style */}
+      <label htmlFor="name" style={{ backgroundColor: 'red', color: 'blue' }}>
+        {/* uSed variable */}
+        {labelname}
+      </label>
+      <input id="name" type="text" />
+    </div>
+  );
+};
+
+// shoe react components
+
+ReactDOM.render(<App />, document.querySelector('#root'));
